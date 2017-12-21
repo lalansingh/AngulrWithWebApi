7609d97d9b0c28f63bcf1034f6d7f4e680a37d4e
IN\lalan.singh <lalan.singh@tavant.com>
2017-12-21 17:08:52 +0530

    Added Unit test case project

---
+++ UnitTestCase/TestBAL/ValidateUserTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BussinessAccess;
+using BussinessAccess.Contract;
+using DataAccess.Repository.Contract;
+using DataModel;
+using Moq;
+using Xunit;
+
+namespace UnitTestCase.TestBAL
+{
+    public class ValidateUserTest
+    {
+        [Fact]
+        public void PassingValidateUserTest()
+        {
+            var mockDal = new Mock<IUserRepository>();
+            mockDal.Setup(c => c.ValidateUser(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new UserModel() {Email = "hilalan.kr@gmail.com"});
+            var bal = new UserManager(mockDal.Object);
+            var result = bal.ValidateUser("lalan", "singh");
+
+            Assert.Equal("hilalan.kr@gmail.com", result.Email);
+        }
+    }
+}
