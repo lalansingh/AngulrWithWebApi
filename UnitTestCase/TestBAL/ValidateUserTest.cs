2ca45501d98da1daa3109da961885237702a353c
IN\lalan.singh <lalan.singh@tavant.com>
2017-12-24 00:09:27 +0530

    test added

--- UnitTestCase/TestBAL/ValidateUserTest.cs
+++ UnitTestCase/TestBAL/ValidateUserTest.cs
@@ -25,5 +25,29 @@ namespace UnitTestCase.TestBAL
 
             Assert.Equal("hilalan.kr@gmail.com", result.Email);
         }
+
+        [Fact]
+        public void FailingValidateUserTest()
+        {
+            var mockDal = new Mock<IUserRepository>();
+            mockDal.Setup(c => c.ValidateUser(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new UserModel() { Email = "hilalan.kr@gmail.com" });
+            var bal = new UserManager(mockDal.Object);
+            var result = bal.ValidateUser("lalan", "singh");
+
+            Assert.NotEqual("hilala.kr@gmail.com", result.Email);
+        }
+
+        [Fact]
+        public void FailedValidateUserTest()
+        {
+            var mockDal = new Mock<IUserRepository>();
+            mockDal.Setup(c => c.ValidateUser(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new UserModel() { Email = "hilalan.kr@gmail.com" });
+            var bal = new UserManager(mockDal.Object);
+            var result = bal.ValidateUser("lalan", "singh");
+
+            Assert.Equal("hilala.kr@gmail.com", result.Email);
+        }
     }
 }
